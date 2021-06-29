f3aef58f5e42849cc11b847c807be5598e113f61
Daniel Shaw <daniel_shaw@shi.com>
2021-06-29 17:08:20 -0400

    notes

--- daily_update.sql
+++ daily_update.sql
@@ -10,7 +10,24 @@ CREATE OR REPLACE FUNCTION public.daily_update(
     VOLATILE PARALLEL UNSAFE
 AS $BODY$
 begin
+/*
+1. All items have a SellIn value which denotes the number of days we have to sell the item
+2. All items have a Quality value which denotes how valuable the item is
+3. At the end of each day our system lowers both values for every item
+Pretty simple, right? Well this is where it gets interesting:
 
+4. Once the sell by date has passed, Quality degrades twice as fast
+5. The Quality of an item is never negative
+6. "Aged Brie" actually increases in Quality the older it gets
+7. The Quality of an item is never more than 50
+8. "Sulfuras", being a legendary item, never has to be sold or decreases in Quality
+9. "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+   Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+   Quality drops to 0 after the concert
+*/
+	-- 3. The quality of all items decreases over time, with the exceptions below
+	-- decrement quality for all items except 'Sulfuras', 'Backstage passes to a TAFKAL80ETC concert', and 'Aged Brie'
+	-- also ignore any items with a quality of 0
 	UPDATE item
 	SET
 		quality = quality - 1
@@ -20,6 +37,9 @@ begin
 		AND name <> 'Sulfuras, Hand of Ragnaros'
 	;
 
+	-- 9. Quality increases by two when there are 10 days or less on sellIn
+	-- increase quality of 'Backstage passes to a TAFKAL80ETC concert' when quality is less than 50 and
+	-- the sellIn date is under 11
 	UPDATE item
 	SET
 		quality = quality + 1
@@ -31,6 +51,9 @@ begin
 	  AND quality < 50
 	;
 
+	-- 9. Quality increases by three when there are 5 days or less in sellIn
+	-- increase quality of 'Backstage passes to a TAFKAL80ETC concert' (possibly again) when quality is less than 50 and
+	-- the sellIn date is under 6
 	UPDATE item
 	SET
 		quality = quality + 1
@@ -42,6 +65,8 @@ begin
 	  AND quality < 50
 	;
 
+	-- 3. sellIn decrements by one.
+	-- decrement all sellIn values except for Sulfuras
 	UPDATE item
 	SET
 		sellIn = sellIn - 1
@@ -49,6 +74,9 @@ begin
 	  AND  name <> 'Sulfuras, Hand of Ragnaros'
 	;
 
+	-- 4. quality decreases twice as fast when the sellIn date falls below 0
+	-- decrement quality for all items except 'Sulfuras', 'Backstage passes to a TAFKAL80ETC concert', and 'Aged Brie'
+	-- when sellIn is less than 0 and quality is > 0
 	UPDATE item
 	SET
 		quality = quality - 1
@@ -60,6 +88,9 @@ begin
 	  AND name <> 'Sulfuras, Hand of Ragnaros'
 	;
 
+	-- 9. Quality drops to 0 after the concert has passed
+	-- zero out quality for all 'Backstage passes to a TAFKAL80ETC concert'
+	-- once the sellIn is less than 0
 	UPDATE item
 	SET
 		quality = quality - quality
@@ -69,6 +100,9 @@ begin
 	  AND NOT (name <> 'Backstage passes to a TAFKAL80ETC concert')
 	;
 
+	-- 6. and 9. 'Aged brie' and 'Backstage passes to a TAFKAL80ETC concert' increase in value the older they get
+	-- increase quality of 'Aged Brie' (possibly again) when quality is less than 50 and
+	-- the sellIn date is under 0
 	UPDATE item
 	SET
 		quality = quality + 1
